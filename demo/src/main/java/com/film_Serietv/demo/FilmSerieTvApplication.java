5f428100ebfd77d47f753cba905bdd177dea5f44
FrancescoSardo <francescosardo91@gmail.com>
2023-10-16 16:34:31 +0200

    setup
    setup

---
+++ demo/src/main/java/com/film_Serietv/demo/FilmSerieTvApplication.java
@@ -0,0 +1,13 @@
+package com.film_Serietv.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmSerieTvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmSerieTvApplication.class, args);
+	}
+
+}
