10109dc3ea9ed6dba954cefc5d5d5fd1630aa4a0
FrancescoSardo <francescosardo91@gmail.com>
2023-10-19 11:27:23 +0200

    setup google login funzionante

--- demo/src/main/java/com/film_Serietv/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/film_Serietv/demo/config/SecurityConfig.java
@@ -5,17 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
  
  @Bean
  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-  http.csrf().disable()
-   .authorizeRequests()
-   .anyRequest().authenticated().and().httpBasic();
-    
+   
+   http
+     .authorizeRequests(auth ->{
+         auth.requestMatchers("/" ).permitAll();
+         auth.anyRequest().authenticated();
+     })
+     .oauth2Login(withDefaults())
+     .formLogin(withDefaults());
+
+
     return http.build();
 
  }
