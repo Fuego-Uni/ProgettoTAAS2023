727b3757ee8dcafef640cbddcaf4fd97e12b5d17
FrancescoSardo <francescosardo91@gmail.com>
2023-10-25 17:03:22 +0200

    google login token ok

--- reviewservice/src/main/java/com/progettotaas/reviewservice/controller/reviewController.java
+++ reviewservice/src/main/java/com/progettotaas/reviewservice/controller/reviewController.java
@@ -1,11 +1,13 @@
 package com.progettotaas.reviewservice.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/review")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class reviewController {
  @GetMapping("/hello")
  public String hello() {
