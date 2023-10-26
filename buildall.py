52032932c14e2e4391137c4a1408ba6774c04175
Tomm2000 <38072918+tomm2000@users.noreply.github.com>
2023-10-26 10:37:48 +0200

    python build

---
+++ buildall.py
@@ -0,0 +1,31 @@
+
+import os
+import sys
+import time
+from os import walk
+
+f = []
+for (dirpath, dirnames, filenames) in walk('.'):
+  f.extend(dirnames)
+  break
+
+# filter only directories that start with service_
+f = filter(lambda x: x.startswith('service_'), f)
+
+# start timer
+start = time.time()
+
+# build all the directories
+for d in f:
+  print("=====================================")
+  print("Building " + d)
+  print("=====================================")
+  os.chdir(d)
+  os.system('mvn package')
+  os.chdir('..')
+
+# end timer, print in seconds
+end = time.time()
+print("=====================================")
+print(f"Total time: {round(end - start)} seconds")
+print("=====================================")
