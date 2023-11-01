c808ecea4628b86db91c0bbea7ae01e31c91d1a2
FrancescoSardo <francescosardo91@gmail.com>
2023-11-01 11:56:01 +0100

    friends table

--- service-auth/src/main/java/com/notflix/serviceauth/controller/AuthController.java
+++ service-auth/src/main/java/com/notflix/serviceauth/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.notflix.serviceauth.controller;
 
+import java.util.Optional;
 import com.google.gson.Gson;
+import com.notflix.serviceauth.entity.UserEntity;
+import com.notflix.serviceauth.entity.UserRole;
 import com.notflix.serviceauth.repository.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,58 +24,30 @@ public class AuthController {
     public UserEntityRepository userEntityRepository;
 
     @PostMapping("/authenticate")
-    public ResponseEntity<String> authenticate(HttpServletRequest request, @RequestBody String userdata) {
-//        check if the user exists in the database
-
+    public ResponseEntity<String> authenticate(HttpServletRequest request, @RequestBody AuthRequest userdata) {
         System.out.println("userdata: " + userdata);
+        //ResponseEntity.ok("User authenticated successfully");
+        String email = userdata.email;
+        Optional<UserEntity> user = userEntityRepository.findByEmail(email);
+
+        if (user.isEmpty()) {
+            var newUser = new UserEntity();
+            newUser.setEmail(email);
+            newUser.setName(userdata.name);
+            newUser.setRole(UserRole.ROLE_USER);
+            userEntityRepository.save(newUser);
+        }
 
         return ResponseEntity.ok("User authenticated successfully");
-//        String email = userdata.email;
-//        Optional<UserEntity> user = userEntityRepository.findByEmail(email);
-//
-//        if (user.isEmpty()) {
-//            var newUser = new UserEntity();
-//            newUser.setEmail(email);
-//            newUser.setName(userdata.name);
-//            newUser.setRole(UserRole.ROLE_USER);
-//            userEntityRepository.save(newUser);
-//        }
-//
-//        return ResponseEntity.ok("User authenticated successfully");
+    }
+    @PostMapping("/addFirend")
+    public ResponseEntity<String> addFriend(HttpServletRequest request, @RequestBody AuthRequest userdatafirend){
+        // 
+        String header = request.getHeader("Authorization");
+        
+
+
     }
 
-//    @PostMapping("/register")
-//    public ResponseEntity<String> register(@RequestBody AuthRequest authrequest) {
-//        String email = authrequest.email ;
-//        String name = authrequest.name ;
-//
-//        var user = new UserEntity();
-//        user.setEmail(email);
-//        user.setName(name);
-//        user.setRole(UserRole.ROLE_USER);
-//
-//        userEntityRepository.save(user);
-//
-//
-//        return ResponseEntity.ok("User registered successfully");
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(HttpServletRequest request) {
-//        Enumeration<String> headerNames = request.getHeaderNames();
-//        boolean hasAuthorization = false;
-//        while (headerNames.hasMoreElements()) {
-//            String headerName = headerNames.nextElement();
-//            if (headerName.equalsIgnoreCase("Authorization")) {
-//                hasAuthorization = true;
-//                break;
-//            }
-//        }
-//        if (hasAuthorization) {
-//            return ResponseEntity.ok("Login");
-//        } else {
-//            return ResponseEntity.status(HttpStatus.SC_UNAUTHORIZED).body("Authorization header not found");
-//        }
-//    }
 
 }
--- service-auth/src/main/java/com/notflix/serviceauth/entity/UserEntity.java
+++ service-auth/src/main/java/com/notflix/serviceauth/entity/UserEntity.java
@@ -1,9 +1,12 @@
 package com.notflix.serviceauth.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="user_entity")
+@Table(name = "user_entity")
 public class UserEntity {
  @Id
  @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,5 +19,22 @@ public class UserEntity {
  @Column(name = "role")
  @Enumerated(EnumType.STRING)
  private UserRole role;
+
+ //
+ @ManyToMany
+ @JoinTable(name = "user_friends", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "friend_id"))
+ private List<UserEntity> friends = new ArrayList<>();
+
+
+ // setters
+ public void setName(String name) {
+  this.name = name;
+ }
+ public void setEmail(String email) {
+  this.email = email;
+ }
+ public void setRole(UserRole role) {
+  this.role = role;
+ }
 
 }
